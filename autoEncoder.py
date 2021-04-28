e47ce8c56db1e3d20888ff5c9acd7fc9ad320ebf
ahmed alnouby <qwertyse122@gmail.com>
2021-04-28 09:21:10 +0200

    IN progress to add dynamic weights

--- autoEncoder.py
+++ autoEncoder.py
@@ -19,63 +19,71 @@ batch_size=256
 # x, y = input_data.get_dataset()
 # X = np.expand_dims(x, axis=2)
 # np.save("inputs.npy",X)
-# np.save("labels.npy")
+# np.save("labels.npy",y)
 
 #######################################################################################################
-X=np.load("inputs.npy")
+def create_auto_encoder():
+    # Building the network
+    channel_size = [23, 32, 64, 64, 256, 256,256, 256,64, 64, 32,23]
+    # betas=[uniform (shape=[1,channel_size[i]]) for i in range(len(channel_size))]
+    betas =  [tf.Variable(0.0 * tf.ones(channel_size[i]), name=f'beta_{i}') for i in range(len(channel_size))]
+    # print(betas[0])
 
-print(X.shape)
-# print(X[0],len(X),len(X[0]),len(X[0][0]))
-X=X.reshape([-1,8, 1, 23])
-# print(X[0],len(X),len(X[0]),len(X[0][0]))
-
-X, X_test, Y, Y_test = train_test_split(X, X, test_size=0.33, random_state=42)
-print(X.shape)
-print(X_test.shape)
-print(Y.shape)
-print(Y_test.shape)
-# print(len(X),len(X[0]),len(X[0][0]),X.shape)
-# print(len(X_test),len(X_test[0]),len(X_test[0][0]),X_test.shape)
-# print(len(Y),len(Y[0]),len(Y[0][0]),Y.shape)
-# print(len(Y_test),len(Y_test[0]),len(Y_test[0][0]),Y_test.shape)
-# Building the network
-
-channel_size = [23, 32, 64, 64, 256, 256,256, 256,64, 64, 32,23]
-# betas=[uniform (shape=[1,channel_size[i]]) for i in range(len(channel_size))]
-betas =  [tf.Variable(0.0 * tf.ones(channel_size[i]), name=f'beta_{i}') for i in range(len(channel_size))]
-# print(betas[0])
-
-AE = input_data(shape=[None,8, 1, 23], name='input')
-for i in range(len(channel_size)):
-    AE = conv_2d(AE,channel_size[i], [1, 3],bias=False,activation=None,name=f"convEncoder_{i}")
-    AE = batch_normalization(AE,decay=0,name=f"BatchNormalizeEncoder_{i}")
-    # AE = AE + betas[i]
-    # AE = relu(AE)
-    AE=sigmoid(AE)
+    AE = input_data(shape=[None,8, 1, 23], name='input')
+    for i in range(len(channel_size)):
+        AE = conv_2d(AE,channel_size[i], [1, 3],bias=False,activation=None,name=f"convEncoder_{i}")
+        AE = batch_normalization(AE,decay=0,name=f"BatchNormalizeEncoder_{i}",trainable=False)#,trainable=False
+        # AE = AE + betas[i]
+        # AE = relu(AE)
+        AE=sigmoid(AE)
 
 
-AE = regression(AE, optimizer='adam', learning_rate=0.0001, loss='categorical_crossentropy', name='target')
+    AE = regression(AE, optimizer='adam', learning_rate=0.0001, loss='categorical_crossentropy', name='target')
 
 
-# Training the network
-model = tflearn.DNN(AE,tensorboard_verbose=0,
-tensorboard_dir = 'MNIST_tflearn_board/',
-checkpoint_path = 'MNIST_tflearn_checkpoints/checkpoint')
+    # Training the network
+    auto_encoder = tflearn.DNN(AE,tensorboard_verbose=0,
+    tensorboard_dir = './TrainingAutoEncoder/autoencoder/AE',
+    checkpoint_path = './TrainingAutoEncoder/autoencoder/AE/checkpoint')
 
-model.fit({'input': X}, {'target': Y}, n_epoch=36,batch_size=batch_size,
-validation_set=({'input': X_test}, {'target': Y_test}),
-snapshot_step=1000,show_metric=True, run_id='convnet_mnist')
+    return auto_encoder
+
+def main():
+
+    X=np.load("inputs.npy")
+
+    print(X.shape)
+    # print(X[0],len(X),len(X[0]),len(X[0][0]))
+    X=X.reshape([-1,8, 1, 23])
+    # print(X[0],len(X),len(X[0]),len(X[0][0]))
+
+    X, X_test, Y, Y_test = train_test_split(X, X, test_size=0.33, random_state=42)
+    print(X.shape)
+    print(X_test.shape)
+    print(Y.shape)
+    print(Y_test.shape)
+    # print(len(X),len(X[0]),len(X[0][0]),X.shape)
+    # print(len(X_test),len(X_test[0]),len(X_test[0][0]),X_test.shape)
+    # print(len(Y),len(Y[0]),len(Y[0][0]),Y.shape)
+    # print(len(Y_test),len(Y_test[0]),len(Y_test[0][0]),Y_test.shape)
+
+    model =create_auto_encoder()
+
+    model.fit({'input': X}, {'target': Y}, n_epoch=36,batch_size=batch_size,
+    validation_set=({'input': X_test}, {'target': Y_test}),
+    snapshot_step=1000,show_metric=True, run_id='convnet_mnist')
 
 
 
-model.save("./TrainingAutoEncoder/model.tfl")
+    model.save("./TrainingAutoEncoder/autoencoder/model.tfl")
 
-df=pd.DataFrame(X[0].reshape(8, 23))
-df.to_csv("./TrainingAutoEncoder/inputs.csv")
+    df=pd.DataFrame(X[0].reshape(8, 23))
+    df.to_csv("./TrainingAutoEncoder/inputs.csv")
 
-encode_decode = model.predict(X[0].reshape(1,8, 1, 23))
-output=np.array(encode_decode)
-output[:] = output[:]>0.5
-df=pd.DataFrame(output.reshape(8, 23))
-df.to_csv("./TrainingAutoEncoder/outputs.csv")
+    encode_decode = model.predict(X[0].reshape(1,8, 1, 23))
+    output=np.array(encode_decode)
+    output[:] = output[:]>0.5
+    df=pd.DataFrame(output.reshape(8, 23))
+    df.to_csv("./TrainingAutoEncoder/outputs.csv")
 
+# main()
--- classification.py
+++ classification.py
@@ -12,6 +12,8 @@ import pandas as pd
 import numpy as np
 batch_size=256
 
+from autoEncoder import create_auto_encoder
+
 
 # file_path = './paper_data-classification/paper_data/ontar/hct116_hart.episgt'
 # # file_path = './examples/eg_cls_on_target.episgt'
@@ -21,6 +23,44 @@ batch_size=256
 # np.save("file.npy",X)
 # np.save()
 #######################################################################################################
+
+# tf.reset_default_graph()
+# auto_encoder = create_auto_encoder()
+# auto_encoder.load("./TrainingAutoEncoder/autoencoder/model.tfl")
+
+# array_of_weights_conv =[]
+
+# array_of_weights_BN =[]
+# tf222= tf.get_default_graph()
+
+# encoder_channel_size = [23, 32, 64, 64, 256, 256]
+
+def get_all_tensor_names(filename):
+    l=[i.name for i in tf.get_default_graph().get_operations()]
+    # l = [tensor.name for tensor in tf.get_default_graph().as_graph_def().node]
+    a_file = open(filename, "w")
+    np.savetxt(a_file, l, delimiter=',', fmt='%s')
+    a_file.close()
+
+# # get_all_tensor_names()
+# get_all_tensor_names("test.txt")
+
+# for i in range(len(encoder_channel_size)):
+#     array_of_weights_conv.append( tf.get_default_graph().get_tensor_by_name(f"convEncoder_{i}/Conv2D:0").W)
+#     # array_of_weights_BN.append(   tf.get_default_graph().get_tensor_by_name(f"BatchNormalizeEncoder_{i}/cond/Identity_1:0").W)
+
+# from tensorflow.python.tools import inspect_checkpoint as chkp
+# chkp.print_tensors_in_checkpoint_file("./TrainingAutoEncoder/autoencoder/model.tfl", tensor_name=None, all_tensors=True)
+
+
+
+
+
+
+
+
+# ########################################################################################################################
+tf.reset_default_graph()
 X=np.load("inputs.npy")
 y=np.load("labels.npy")
 print(X.shape)
@@ -43,6 +83,10 @@ print(Y_test.shape)
 # print(len(X_test),len(X_test[0]),len(X_test[0][0]),X_test.shape)
 # print(len(Y),len(Y[0]),len(Y[0][0]),Y.shape)
 # print(len(Y_test),len(Y_test[0]),len(Y_test[0][0]),Y_test.shape)
+
+
+
+
 # Building the network
 channel_size=[23, 32, 64, 64, 256, 256,512, 512, 1024, 2]
 # betas=[uniform (shape=[1,channel_size[i]]) for i in range(len(channel_size))]
@@ -53,7 +97,7 @@ AE = input_data(shape=[None,8, 1, 23], name='input')
 encoder_channel_size = [23, 32, 64, 64, 256, 256]
 for i in range(len(encoder_channel_size)):
     AE = conv_2d(AE,encoder_channel_size[i], [1, 3],bias=False,activation=None,name=f"convEncoder_{i}")
-    AE = batch_normalization(AE,decay=0,name=f"BatchNormalizeEncoder_{i}")
+    AE = batch_normalization(AE,decay=0,name=f"BatchNormalizeEncoder_{i}",trainable=False)
     AE=sigmoid(AE)
     # AE = AE + betas[i]
     # AE = relu(AE)
@@ -61,18 +105,18 @@ for i in range(len(encoder_channel_size)):
 cls_channel_size = [512, 512, 1024,2]
 for i in range(len(cls_channel_size)-1):
     if i==0:
-        AE = conv_2d(AE,cls_channel_size[i], [1, 3], strides=2,bias=False,activation=None,name=f"convCls_{i}")
+        AE = conv_2d(AE,cls_channel_size[i], [1, 3], strides=2,bias=False,activation=None,name=f"convCls_{i}",restore=False)
     if i==1:
-        AE = conv_2d(AE,cls_channel_size[i], [1, 3],bias=False,activation=None,name=f"convCls_{i}")
+        AE = conv_2d(AE,cls_channel_size[i], [1, 3],bias=False,activation=None,name=f"convCls_{i}",restore=False)
     if i==2:
-        AE = conv_2d(AE,cls_channel_size[i], [1, 3],bias=False,activation=None,name=f"convCls_{i}")#,padding='VALID'
-    AE = batch_normalization(AE,decay=0.99,name=f"BatchNormalizeCls_{i}")
+        AE = conv_2d(AE,cls_channel_size[i], [1, 3],bias=False,activation=None,name=f"convCls_{i}",restore=False)#,padding='VALID'
+    AE = batch_normalization(AE,decay=0.99,name=f"BatchNormalizeCls_{i}",restore=False)
     # AE = AE + betas[i]
     # AE = relu(AE)
     AE=sigmoid(AE)
 
 # AE=AE.reshape([None,1,1,2])
-AE = conv_2d(AE,cls_channel_size[3], [1, 1],bias=False,activation=None,name="convCls_3")
+AE = conv_2d(AE,cls_channel_size[3], [1, 1],bias=False,activation=None,name="convCls_3",restore=False)
 # AE = AE + betas[i]
 AE = tf.nn.softmax(AE)
 
@@ -86,21 +130,38 @@ AE = tf.squeeze(AE, axis=[1, 2])[:, 1]
 print(AE.shape)
 # AE = tf.squeeze(AE, axis=[1, 2])[:, 1]
 
-AE = regression(AE, optimizer='adam', learning_rate=0.0001, loss='categorical_crossentropy', name='target')
+AE = regression(AE, optimizer='adam', learning_rate=0.0001
+    , loss='categorical_crossentropy', name='target', restore=False)
 
 
 # Training the network
 model = tflearn.DNN(AE,tensorboard_verbose=0,
-tensorboard_dir = 'MNIST_tflearn_board/',
-checkpoint_path = 'MNIST_tflearn_checkpoints/checkpoint')
+tensorboard_dir = './TrainingAutoEncoder/classification/AE',
+checkpoint_path = './TrainingAutoEncoder/classification/AE/checkpoint')
+
+get_all_tensor_names("lol.txt")
+
+# weight saving and loading resources
+# Keywords:
+# dynamically assign weights to a layer
+# initialize layer weights from a numpy array
+# how to write a layer initializer function in tensorflow
+# what is a feed_dict in tensorflow
+# Process:
+# extract weights from layer as numpy array
+# Either initialize or dynamically assign weights to new layers
+model.load("./TrainingAutoEncoder/autoencoder/model.tfl",weights_only=True)
+
+# encoder_channel_size = [23, 32, 64, 64, 256, 256]
+# for i in range(len(encoder_channel_size)):
+#     model.set_weights(tf.get_default_graph().get_tensor_by_name(f"convEncoder_{i}:0").W
+#     , array_of_weights_conv[i])
+#     model.set_weights(tf.get_default_graph().get_tensor_by_name(f"BatchNormalizeEncoder_{i}:0").W
+#     , array_of_weights_BN[i])
 
 
-# model.load("./TrainingAutoEncoder/autoencoder/model.tfl")
 
-
-
-
-model.fit({'input': X}, {'target': Y}, n_epoch=120,batch_size=batch_size,
+model.fit({'input': X}, {'target': Y}, n_epoch=25,batch_size=batch_size,
 validation_set=({'input': X_test}, {'target': Y_test}),
 snapshot_step=1000,show_metric=True, run_id='convnet_mnist')
 
