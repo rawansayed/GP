b4d3a5d1cf6f6f56f6cbd619e281c15642bddd8a
SaraAdel540 <sarahadel540@gmail.com>
2021-05-24 16:36:22 +0200

    regression

--- Translating_data/dataenterpertation.py
+++ Translating_data/dataenterpertation.py
@@ -2,11 +2,23 @@ from utils import Episgt
 import numpy as np
 
 
-file_path = './database/paper_data-classification/paper_data/ontar/hct116_hart.episgt'
+# ------------- For Classification ------------- #
+
+file_path1 = './database/paper_data-classification/paper_data/ontar/hct116_hart.episgt'
 # file_path = './examples/eg_cls_on_target.episgt'
-input_data = Episgt(file_path, num_epi_features=4, with_y=True)
-x, y = input_data.get_dataset()
-X = np.expand_dims(x, axis=2)
+input_data = Episgt(file_path1, num_epi_features=4, with_y=True)
+x_1, y_1 = input_data.get_dataset()
+X_1 = np.expand_dims(x_1, axis=2)
 # X = X.transpose([0, 2, 3, 1])
-np.save("inputs.npy",X)
-np.save("labels.npy",y)
+np.save("inputs.npy",X_1)
+np.save("labels.npy",y_1)
+
+
+# --------------- For Regression --------------- #
+
+file_path2 = './database/paper_data-regression/paper_data2/ontar/hct116.repisgt'
+input_data = Episgt(file_path2, num_epi_features=4, with_y=True)
+x_2, y_2 = input_data.get_dataset()
+X_2 = np.expand_dims(x_2, axis=2)
+np.save("inputs_reg.npy",X_2)
+np.save("labels_reg.npy",y_2)
